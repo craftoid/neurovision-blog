f4a6ecfe5906c4140df87066421a4867316695f0
Martin Schneider <craftoid@k2g2.org>
2013-03-03 13:06:03 +0100

    Fixing twitter theme - tables, feeds and jquery
    - Fixing feeds
    
      Make sure the twitter theme only creates links for rss and atom feeds,
      if we have configured them
    
    - Fixing tables
    
      Include jquery and a custom script to create twitter-bootstrap
      table layout by default
    
    - Fixing scripts
    
      We can now add custom javascript to scripts.js whenever we need.

---
+++ assets/themes/twitter/js/script.js
@@ -0,0 +1,9 @@
+
+$(function() {
+	// decorate all tables so they look nice
+	$("table").addClass("table")
+		.addClass("table-striped")
+		.addClass("table-condensed")
+		.addClass("table-bordered")
+	;
+});
